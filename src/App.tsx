3527b1e000e509032460026b4030bf6831f32231
unknown <jec.dev.22@gmail.com>
2023-07-26 23:43:02 -0300

    Agregar styles de Dark Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import ProviderLightTheme from './contexts/ProviderLightTheme'
 import ProviderTodo from './contexts/ProviderTodo'
 import './styles/global.css'
+import './styles/theme.css'
 
 const App = () => {
   return (
