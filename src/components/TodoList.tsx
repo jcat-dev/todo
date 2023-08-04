636d445460475eff22143dd23f9781376510d07f
unknown <jec.dev.22@gmail.com>
2023-08-03 22:40:14 -0300

    Agregar nueva funcionalidad Drag and Drop

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,6 @@
 import { TodoContext } from '../contexts/ProviderTodo'
 import { useContext } from 'react'
+import { DragDropContext, Draggable, DropResult, Droppable } from 'react-beautiful-dnd'
 import iconCheck from '../assets/images/icon-check.svg'
 import iconCross from '../assets/images/icon-cross.svg'
 import '../styles/todoList.css'
@@ -8,69 +9,103 @@ const TodoList: React.FC = () => {
   const {
     todoList,
     completeTodo,
-    deleteTodoByID
+    deleteTodoByID,
+    sortList
   } = useContext(TodoContext)
   
+  const handleDragAndDrop = (result: DropResult) => {
+    if (!result.destination) return
+
+    sortList(result.source.index, result.destination.index)
+  }
+
   return (
-    <ul 
-      className='todo-list'
+    <DragDropContext
+      onDragEnd={(result) => handleDragAndDrop(result)}
     >
-      {
-        todoList.map((value, index) => {
-          if (index <= 5) 
-            return (
-              <li 
-                key={value._id}
-                className='todo-list__item'
-              >                              
-                <div
-                  className={
-                    value.completed
-                      ? 'todo-list__item-completed todo-list__item-completed--bg-active'
-                      : 'todo-list__item-completed'
-                  }
-                >
-                  <button            
-                    className={
-                      value.completed 
-                        ? 'completed-btn completed-btn--bg-active'
-                        : 'completed-btn'                      
-                    }         
-                    onClick={() => completeTodo(value)}
-                  >
-                    <img 
-                      hidden={!value.completed}
-                      src={iconCheck} 
-                      alt="icon check" 
-                    />
-                  </button>   
-                </div>                     
+      <Droppable
+        droppableId='todo'
+      >
+        {
+          (provided) => (         
+            <ul 
+              className='todo-list'
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {
+                todoList.map((value, index) => {
+                  if (index <= 5) 
+                    return (
+                      <Draggable
+                        key={value._id}
+                        draggableId={value._id}
+                        index={index}
+                      >
+                        {
+                          (provided) => (                         
+                            <li 
+                              className='todo-list__item'
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                              ref={provided.innerRef}
+                            >                              
+                              <div
+                                className={
+                                  value.completed
+                                    ? 'todo-list__item-completed todo-list__item-completed--bg-active'
+                                    : 'todo-list__item-completed'
+                                }
+                              >
+                                <button            
+                                  className={
+                                    value.completed 
+                                      ? 'completed-btn completed-btn--bg-active'
+                                      : 'completed-btn'                      
+                                  }         
+                                  onClick={() => completeTodo(value)}
+                                >
+                                  <img 
+                                    hidden={!value.completed}
+                                    src={iconCheck} 
+                                    alt="icon check" 
+                                  />
+                                </button>   
+                              </div>                     
                   
-                <p 
-                  className={
-                    value.completed
-                      ? 'todo-list__item-title todo-list__item-title--completed'
-                      : 'todo-list__item-title'
-                  }
-                >
-                  {value.title}
-                </p>
+                              <p 
+                                className={
+                                  value.completed
+                                    ? 'todo-list__item-title todo-list__item-title--completed'
+                                    : 'todo-list__item-title'
+                                }
+                              >
+                                {value.title}
+                              </p>
                   
-                <button 
-                  className='todo-list__item-delete'
-                  onClick={() => deleteTodoByID(value._id)}
-                >
-                  <img 
-                    hidden
-                    src={iconCross} 
-                    alt="icon cross" 
-                  />
-                </button>                              
-              </li>
-            )
-        })
-      }      
-    </ul>
+                              <button 
+                                className='todo-list__item-delete'
+                                onClick={() => deleteTodoByID(value._id)}
+                              >
+                                <img 
+                                  hidden
+                                  src={iconCross} 
+                                  alt="icon cross" 
+                                />
+                              </button>                              
+                            </li>
+                          )
+                        }
+                      </Draggable>
+                    )
+                })
+              }  
+              {provided.placeholder}    
+            </ul>
+          )
+        }
+      </Droppable>
+    </DragDropContext>
   )
 }
 
