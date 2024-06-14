1a75a6a60967dbbcc1d3dec59b64af14cf14d131
unknown <jec.dev.22@gmail.com>
2024-06-13 21:57:56 -0300

    Eliminar ProviderLightTheme.tsx de /contexts

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import ProviderLightTheme from './contexts/ProviderLightTheme'
 import ProviderTodo from './contexts/ProviderTodo'
 import './styles/global.css'
 import './styles/theme.css'
 
 const App = () => {
   return (
-    <ProviderLightTheme>
-      <ProviderTodo />
-    </ProviderLightTheme>
+    <ProviderTodo />
   )
 }
 
