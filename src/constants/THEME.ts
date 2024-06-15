8f065aa3d8d4fcacd4deef751d0c236a44559099
unknown <jec.dev.22@gmail.com>
2024-06-14 22:31:06 -0300

    Agregar /constants

---
+++ src/constants/THEME.ts
@@ -0,0 +1,3 @@
+export const THEME_KEY = 'theme'
+export const THEME_LIGHT_VALUE = 'light-mode'
+export const THEME_DARK_VALUE = 'dark-mode'
---
+++ src/constants/TODO_TYPE.ts
@@ -0,0 +1,4 @@
+export const TODO_TYPE_KEY = 'todo-type'
+export const TODO_ALL_VALUE = 'all'
+export const TODO_ACTIVE_VALUE = 'active'
+export const TODO_COMPLETED_VALUE = 'completed'
