d2f6b8d70122cc3a6a9f23877070c4e340095b28
unknown <jec.dev.22@gmail.com>
2024-07-03 17:29:11 -0300

    Refactorizar y Actualizar fetchTodo.ts en /utils

--- src/utils/fetchTodo.ts
+++ src/utils/fetchTodo.ts
@@ -1,39 +1,20 @@
-import { ResponseTodo } from '../types/Response'
-type Method = 'GET' | 'DELETE'| 'POST' | 'PUT' 
+import { FetchMethod, FetchResponse } from '../types/Fetch'
+const API_TODO = String(import.meta.env.VITE_API_TODO)
 
-const setGet = async (url?: string) => {
-  return await setFetch(url ?? '', 'GET')
-}
+export const fetchTodo = async <T = FetchResponse>(url: string, method: FetchMethod, body?: object): Promise<T> => {
+  try {
+    const result = await fetch(API_TODO + url, {
+      method: method,
+      body: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
 
-const setDelete = async (url?: string) => {
-  return await setFetch(url ?? '', 'DELETE')
-}
-
-const setPost = async <T>(body: T, url?: string) => {
-  return await setFetch(url ?? '', 'POST', body)
-}
-
-const setUpdate = async <T>(body: T, url?: string,) => {
-  return await setFetch(url ?? '', 'PUT', body)
-}
-
-const setFetch = async <T>(url: string, method: Method, body?: T) => {
-  const result = await fetch(String(import.meta.env.VITE_API_TODO) + url, {
-    method: method,
-    body: JSON.stringify(body),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-
-  const data: ResponseTodo = await result.json()
-  return data
-}
-
-
-export default {
-  setGet,
-  setDelete,
-  setPost,
-  setUpdate,
+    return await result.json() as T
+  } catch (error) {
+    return {
+      status: false,
+    } as T
+  }
 }
