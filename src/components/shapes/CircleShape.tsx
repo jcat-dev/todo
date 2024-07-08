d9e37b8a6abbb751e779ca0a10640cdc0abb90c2
unknown <jec.dev.22@gmail.com>
2024-07-08 20:48:15 -0300

    Cambiar className a opcional props en /components/shapes/CircleShape.tsx

--- src/components/shapes/CircleShape.tsx
+++ src/components/shapes/CircleShape.tsx
@@ -1,13 +1,13 @@
 import styles from './circleShape.module.css'
 
 interface Props {
-  className: string
+  className?: string
   children?: React.ReactNode
 }
 
 const CircleShape: React.FC<Props> = ({className, children}) => {
   return (
-    <span className={`${className} ${styles['circle']}`} >
+    <span className={`${className ?? ''} ${styles['circle']}`} >
       {
         children
       }
